7355ba1793ed61a3f6dde0d779dbdb1a7737442a
Paulo Augusto <pademacena@gmail.com>
2023-06-17 22:23:42 -0300

    fix code smell complete task TODO

--- src/main/java/com/UFF/ProjetoRide/models/Usuario.java
+++ src/main/java/com/UFF/ProjetoRide/models/Usuario.java
@@ -100,37 +100,30 @@ public class Usuario implements Serializable, UserDetails {
 	}
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
 		return new HashSet<GrantedAuthority>();
 	}
 	@Override
 	public String getPassword() {
-		// TODO Auto-generated method stub
 		return this.senha;
 	}
 	@Override
 	public String getUsername() {
-		// TODO Auto-generated method stub
 		return this.email;
 	}
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
 		return true;
 	}
     
